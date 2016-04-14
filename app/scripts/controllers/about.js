58a7a9d9bfa65d69f7c2d3c05d7488c48b4c3f87
DarkArmed <qzdavid@gmail.com>
2016-04-14 21:04:16 +0800

    yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name galleryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the galleryApp
+ */
+angular.module('galleryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name galleryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the galleryApp
+ */
+angular.module('galleryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
